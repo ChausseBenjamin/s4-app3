364fdd196040e2c4dab2cb88c2fd40b49b566acc
LYAM <cous5830@gmail.com>
2025-06-11 23:43:17 -0400

    auuuugh

--- assembly/2e-etage-cous1912-chab1704.reference.asm
+++ assembly/2e-etage-cous1912-chab1704.reference.asm
@@ -26,61 +26,46 @@ main:
     # pas de fonctions, donc pas de protection des registres
     li $s0, 0               # j = 0
     li $s2, N
-    nop
-    nop
+    #nop
 
 boucle_externe:
     beq  $s0, $s2, finBoucleExterne
-    nop
-    nop
-    nop
+    #nop
     li   $s1, 0             # i = 0
-    nop
-    nop
+    #nop
 
     boucle_interne:
         beq $s1, $s2, finBoucleInterne # for i < 4
-        nop
-        nop
-        nop
+        #nop
         
         # obtenir x[j]
         sll $t4, $s0, 2          # décalage pour adresse en mots
-        nop
-        nop
+        #nop
         lw  $t1, vec_entree($t4) # lecture de x[j]
         
         # obtenir y[i]
         sll $t5, $s1, 2          # décalage pour adresse en mots
-        nop
-        nop
+        #nop
         lw  $t0, vec_sortie($t5) # lecture de y[i]
         
         # Lecture de A[i][j]
         #       indice == i*N + j et N == 4
         sll $t4, $s1, 2     # i*4
-        nop
-        nop
+        #nop
         add $t4, $t4, $s0   # i*4+j
-        nop
-        nop
+        #nop
         sll $t4, $t4, 2     # décalage [i*4+j] (adresse en mots)
-        nop
-        nop
+        #nop
         lw  $t2, mat_A($t4) # lecture de A[i][j]
-        nop
-        nop
+        #nop
 
         multu $t1, $t2      # A[i][j] * x[j]
-        nop
-        nop
+        #nop
         mflo $t1            # récupération des 32 1ers bits
-        nop
-        nop
+        #nop
 
         add $t0, $t0, $t1   # y[i] = y[i] + A[i][j] * x[j]
-        nop
-        nop
+        #nop
         
         sw $t0, vec_sortie($t5) # écriture de y[i]
         
@@ -88,27 +73,17 @@ boucle_externe:
         addi $s1, $s1, 1    # i++
         #nop                 # un seul nop car le jump = 2e operation entre le beq qui lui est concurrant
         j boucle_interne    #
-        nop
-        nop
-        nop
+        #nop
     
 finBoucleInterne:
     addi $s0, $s0, 1        # j++
-    #nop
     j boucle_externe
-    nop
-    nop
-    nop
+    #nop
     
 finBoucleExterne:
     addi $v0, $zero, 10     # fin du programme
-    nop
-    nop
+    #nop
     syscall
-    nop
-    nop
-    nop
-    nop
     
 
 
